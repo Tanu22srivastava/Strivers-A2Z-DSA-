90135e69d6ede05d1a949850f92481f17c719bc1
Tanushree Srivastava <tanushreesrivastava22@gmail.com>
2025-01-18 20:57:41 +0530

    corrected code

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -6,12 +6,12 @@ public class SelectionSort {
         for(int i=0;i<n;i++){
             int mini= i;
             for(int j=i+1;j<n;j++){
-                if(arr[j]<arr[i]){
+                if(arr[j]<arr[mini]){
                     mini=j;
                 }
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+                int temp=arr[mini];
+                arr[mini]=arr[i];
+                arr[i]=temp;
             }
             
         }
